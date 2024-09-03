289a0a57e1bb606e90e63bb520df117857371f45
UrienNolasco <urien.dragon@gmail.com>
2024-09-03 11:09:01 -0300

    feat: add feat until 1h

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,8 @@
 import Header from "@/_components/header"
+import { Avatar, AvatarImage } from "@/_components/ui/avatar"
+import { Badge } from "@/_components/ui/badge"
 import { Button } from "@/_components/ui/button"
+import { Card, CardContent } from "@/_components/ui/card"
 import { Input } from "@/_components/ui/input"
 import { SearchIcon } from "lucide-react"
 import Image from "next/image"
@@ -28,6 +31,31 @@ const Home = () => {
             className="rounded-xl object-cover"
           />
         </div>
+
+        {/* Agendamento */}
+        <Card className="mt-6">
+          <CardContent className="flex justify-between p-0">
+            {/* Esquerda */}
+            <div className="flex flex-col gap-2 py-5 pl-5">
+              <Badge className="w-fit">Confirmado</Badge>
+              <h3 className="text-xl font-bold">Corte de Cabelo</h3>
+
+              <div className="flex items-center gap-2">
+                <Avatar className="h-6 w-6">
+                  <AvatarImage src="https://utfs.io/f/c97a2dc9-cf62-468b-a851-bfd2bdde775f-16p.png" />
+                </Avatar>
+                <p className="text-sm">Barbearia FSW</p>
+              </div>
+            </div>
+
+            {/* Direita */}
+            <div className="flex flex-col items-center justify-center border-l-2 border-solid px-5">
+              <p className="text-sm">Setembro</p>
+              <p className="text-2xl">05</p>
+              <p className="text-sm">20:00</p>
+            </div>
+          </CardContent>
+        </Card>
       </div>
     </div>
   )
