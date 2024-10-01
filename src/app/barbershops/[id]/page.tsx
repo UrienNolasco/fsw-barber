1c6e63aaeee552e2b110b90e10a975fb305a1ca1
UrienNolasco <urien.dragon@gmail.com>
2024-10-01 13:29:45 -0300

    feat: ensure barbershop services are show on barbershop page

--- src/app/barbershops/[id]/page.tsx
+++ src/app/barbershops/[id]/page.tsx
@@ -1,4 +1,5 @@
 import { Button } from "@/_components/ui/button"
+import ServiceItem from "@/_components/ui/service-item"
 import { db } from "@/_lib/prisma"
 import { ChevronLeftIcon, MapPinIcon, MenuIcon, StarIcon } from "lucide-react"
 import Image from "next/image"
@@ -16,6 +17,9 @@ const BarbershopPage = async ({ params }: BarbershopPageProps) => {
   const barbershop = await db.barbershop.findUnique({
     where: {
       id: params.id,
+    },
+    include: {
+      services: true,
     },
   })
 
@@ -70,6 +74,15 @@ const BarbershopPage = async ({ params }: BarbershopPageProps) => {
       <div className="space-y-3 border-b border-solid p-5">
         <h2 className="text-xs font-bold uppercase text-gray-400">Sobre nós</h2>
         <p className="text-justify text-sm">{barbershop?.description}</p>
+      </div>
+
+      <div className="space-y-3 p-5">
+        <h2 className="text-xs font-bold uppercase text-gray-400">Serviços</h2>
+        <div className="space-y-3">
+          {barbershop.services.map((service) => (
+            <ServiceItem key={service.id} service={service} />
+          ))}
+        </div>
       </div>
     </div>
   )
