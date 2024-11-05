24984bd4e8470d8aaba40efe269e183adc6fec1c
UrienNolasco <urien.dragon@gmail.com>
2024-11-05 10:38:13 -0300

    feat: ensure bookings are listed on bookings page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,17 +4,41 @@ import Header from "@/_components/header"
 import Search from "@/_components/search"
 import { Button } from "@/_components/ui/button"
 import { quickSearchOptions } from "@/_constants/search"
+import { authOptions } from "@/_lib/auth"
 import { db } from "@/_lib/prisma"
+import { getServerSession } from "next-auth"
 import Image from "next/image"
 import Link from "next/link"
 
 const Home = async () => {
+  const session = await getServerSession(authOptions)
   const barbershop = await db.barbershop.findMany({})
   const popularBarbershops = await db.barbershop.findMany({
     orderBy: {
       name: "desc",
     },
   })
+
+  const confirmedBookings = session?.user
+    ? await db.booking.findMany({
+        where: {
+          userId: (session.user as any).id,
+          date: {
+            gte: new Date(),
+          },
+        },
+        include: {
+          service: {
+            include: {
+              barbershop: true,
+            },
+          },
+        },
+        orderBy: {
+          date: "asc",
+        },
+      })
+    : []
 
   return (
     <div>
@@ -63,7 +87,15 @@ const Home = async () => {
 
         {/* Agendamento */}
 
-        <BookingItem />
+        <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
+          Agendamentos
+        </h2>
+
+        <div className="flex gap-3 overflow-x-auto [&::-webkit-scrollbar]:hidden">
+          {confirmedBookings.map((booking) => (
+            <BookingItem key={booking.id} booking={booking} />
+          ))}
+        </div>
 
         <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
           Recomendados
